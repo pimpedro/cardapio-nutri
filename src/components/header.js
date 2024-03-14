83591e13ddd03da5706d86f5451e23c3afd582bd
Pedro <pedromurad@gmail.com>
2024-03-14 15:18:41 -0300

    fix quebra de linha header nutri

--- src/components/header.js
+++ src/components/header.js
@@ -24,7 +24,7 @@ const Header = ({nutritionistName, cupomCode, nutritionistImage}) => {
         </div>
         <div className={styles.contentWrap}>
             <div> 
-            <h1 className={styles.contentTitle}>{nutritionistName} e Trela – Seleção de produtos favoritos</h1>
+            <h1 className={styles.contentTitle}>{nutritionistName} e Trela <br/> Seleção de produtos favoritos</h1>
             <p>Quer encontrar produtos das melhores marcas em um só lugar? A Trela te ajuda a comprar alimentos de qualidade para ter uma vida mais equilibrada e comer bem em todas as suas refeições. Eles ainda garantem os melhores preços do mercado e a entrega é sempre grátis.</p>
             <p><strong>Ganhe 20% OFF </strong> na primeira compra usando meu cupom: <strong>{cupomCode}</strong>.</p>
             </div> 
