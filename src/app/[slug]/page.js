e9d1ae3292e8092d180866976383e98797f00d72
Pedro <pedromurad@gmail.com>
2024-03-14 18:52:13 -0300

    arrumando loading

--- src/app/[slug]/page.js
+++ src/app/[slug]/page.js
@@ -8,8 +8,8 @@ import TopBanner from "@/components/banner/top-banner";
 const NuticionistList = async (props) => {
   const productLinks = await fetchProducts(`product-links?populate=*&filters[nutritionists][slug][$eq]=${props.params.slug}&pagination[pageSize]=100`)
   const nutricionist = await fetchProducts(`nutritionists?populate=*&filters[slug][$eq]=${props.params.slug}`)
-  const categories = await fetchProducts(`categories?populate=*&pagination[pageSize]=100`)
-  console.log(nutricionist.data[0].attributes.profile_image.data.attributes.url)
+  const categories = await fetchProducts(`categories?populate=*&pagination[pageSize]=300`)
+  // console.log(nutricionist.data[0].attributes.profile_image.data.attributes.url)
 
   return(
   <div>
