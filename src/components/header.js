afb8267e74a8dd32718ff27b99556a2aaa1db236
Pedro <pedromurad@gmail.com>
2024-03-14 16:14:14 -0300

    arrumando cor de fundo banner

--- src/components/header.js
+++ src/components/header.js
@@ -26,11 +26,11 @@ const Header = ({nutritionistName, cupomCode, nutritionistImage}) => {
             <div> 
             <h1 className={styles.contentTitle}>{nutritionistName} e Trela <br/> Seleção de produtos favoritos</h1>
             <p>Quer encontrar produtos das melhores marcas em um só lugar? A Trela te ajuda a comprar alimentos de qualidade para ter uma vida mais equilibrada e comer bem em todas as suas refeições. Eles ainda garantem os melhores preços do mercado e a entrega é sempre grátis.</p>
-            <p><strong>Ganhe 20% OFF </strong> na primeira compra usando meu cupom: <strong>{cupomCode}</strong>.</p>
+            {/* <p><strong>Ganhe 20% OFF </strong> na primeira compra usando meu cupom: <strong>{cupomCode}</strong>.</p> */}
             </div> 
-            {/* <div className={styles.bannerWrap}>
-              <p><strong>Ganhe 20% OFF </strong> na primeira compra usando meu cupom: <strong>{cupomCode}</strong>.</p>
-            </div> */}
+            <div className={styles.banner}>
+              <span className={styles.bannerWrap}><strong>Ganhe 20% OFF </strong> na primeira compra usando meu cupom: <strong>{cupomCode}</strong>.</span>
+            </div>
         </div>
     </header>
     
