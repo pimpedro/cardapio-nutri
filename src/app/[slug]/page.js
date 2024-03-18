350df3b2772493b6f9896c67315b76d11447e006
Pedro <pedromurad@gmail.com>
2024-03-18 17:48:57 -0300

    fazendo não aparecer undefined

--- src/app/[slug]/page.js
+++ src/app/[slug]/page.js
@@ -6,60 +6,46 @@ import Cover from "@/components/cover/cover";
 import TopBanner from "@/components/banner/top-banner";
 
 const NuticionistList = async (props) => {
-  const productLinks = await fetchProducts(`product-links?pagination[pageSize]=500&populate=*&filters[nutritionists][slug][$eq]=${props.params.slug}&sort[1]=url:asc`)
-  const nutricionist = await fetchProducts(`nutritionists?populate=*&filters[slug][$eq]=${props.params.slug}`)
-  const categories = await fetchProducts(`categories?populate=*&sort[0]=position:asc&pagination[pageSize]=30`)
-  
+  const [productLinks, nutricionist, categories] = await Promise.all([
+    fetchProducts(`product-links?pagination[pageSize]=500&populate=*&filters[nutritionists][slug][$eq]=${props.params.slug}&sort[1]=url:asc`),
+    fetchProducts(`nutritionists?populate=*&filters[slug][$eq]=${props.params.slug}`),
+    fetchProducts(`categories?populate=*&sort[0]=position:asc&pagination[pageSize]=30`)
+  ]);
 
+  const productLinksData = productLinks.data || [];
+  const nutricionistData = nutricionist.data || [];
+  const categoriesData = categories.data || [];
 
-  return(
-  <div >
-    <TopBanner cupomCode={nutricionist.data[0].attributes.cupomcode}/>
-    <Cover/>
-    <div className="container pb-60">
-    <Header nutritionistName={nutricionist.data[0].attributes.Name} 
-    nutritionistImage={nutricionist.data[0].attributes.profile_image.data.attributes.url}
-    ></Header>
-    {
-    categories.data.map((category,index)=>{
-
-        const quantProduts = productLinks.data.filter(product => product && product.attributes && product.attributes.category && product.attributes.category.data&& product.attributes.category.data.attributes && product.attributes.category.data.attributes && product.attributes.category.data.attributes && product.attributes.category.data.attributes.name === `${category.attributes.name}`).length
-        
-
-        if(quantProduts > 0){
-          return ( <div key={index}>
-            <h2 className={styles.category_header} >{category.attributes.name}</h2>
+  return (
+    <div>
+      <TopBanner cupomCode={nutricionistData[0]?.attributes.cupomcode}/>
+      <Cover/>
+      <div className="container pb-60">
+        <Header
+          nutritionistName={nutricionistData[0]?.attributes.Name}
+          nutritionistImage={nutricionistData[0]?.attributes.profile_image?.data?.attributes?.url}
+        />
+        {categoriesData.map((category, index) => {
+          const productsInCategory = productLinksData.filter(product =>
+            product?.attributes?.category?.data?.attributes?.name === category.attributes.name
+          );
+          if (productsInCategory.length > 0) {
+            return (
+              <div key={index}>
+                <h2 className={styles.category_header}>{category.attributes.name}</h2>
                 <div className="row">
-                  {productLinks.data.filter(product => product && product.attributes && product.attributes.category && product.attributes.category.data&& product.attributes.category.data.attributes && product.attributes.category.data.attributes && product.attributes.category.data.attributes && product.attributes.category.data.attributes.name === `${category.attributes.name}`).map((productLink, index)=> 
-                  {
-                    return (
-                        <BookmarkReader key={index} url={`${productLink.attributes.url} `} nutriSlug={nutricionist.data[0].attributes.slug} />
-                      
-                    )
-                  }
-                  )}
-                  </div>
-              </div>)
-
-
-
-        }      
-
-        }
-      )}
+                  {productsInCategory.map((productLink, productIndex) => (
+                    <BookmarkReader key={productIndex} url={`${productLink.attributes.url}`} nutriSlug={nutricionistData[0]?.attributes.slug} />
+                  ))}
+                </div>
+              </div>
+            );
+          }
+          return null;
+        })}
       </div>
     </div>
-  )
-  };
+  );
+};
 
-  // export const generateStaticParams = async () => {
-  // const nutritionistLinks = await fetchProducts(`nutritionists`)
-    
-  //   return nutritionistLinks.data.map((nutritionist) => ({
-  //     slug:nutritionist.attributes.slug
-  //   }
-  //     // console.log(nutritionist.attributes.slug)
-  //   ))
-  // }
-
-export default NuticionistList
+export default NuticionistList;
--- src/components/bookmark/bookmark.js
+++ src/components/bookmark/bookmark.js
@@ -1,19 +1,19 @@
 'use client'
 import { useEffect, useState } from 'react';
 import styles from './bookmark.module.sass';
-import Link from 'next/link'
+import Link from 'next/link';
 import Image from 'next/image';
 
-function BookmarkReader({url, nutriSlug}) {
+function BookmarkReader({ url, nutriSlug }) {
   const [openGraphData, setOpenGraphData] = useState(null);
-
-    const urlprops = url
 
   useEffect(() => {
     const fetchOpenGraphData = async () => {
       try {
-        const url = `${urlprops}`; // Replace with your URL
         const response = await fetch(`/api/opengraph?url=${encodeURIComponent(url)}`);
+        if (!response.ok) {
+          throw new Error('Failed to fetch OpenGraph data');
+        }
         const data = await response.json();
         setOpenGraphData(data);
       } catch (error) {
@@ -22,28 +22,29 @@ function BookmarkReader({url, nutriSlug}) {
     };
 
     fetchOpenGraphData();
-  }, []);
+  }, [url]);
 
+  if (!openGraphData || !openGraphData.title || openGraphData.title === "undefined | Trela") {
+    return null; // Render nothing if data is not available or has invalid title
+  }
 
   return (
     <div className={`${styles.bookmarkWrap} col_2`}>
       <div className={styles.bookmark}>
-        {openGraphData && (
-          <Link
-            href={`${urlprops.trim()}?utm_medium=referral&utm_source=pagnutri&utm_campaign=nutris&utm_content=${nutriSlug.trim()}`}
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            <div className={styles.bookmark_imageWrap}>
-              <div className={styles.bookmark_image}>
-                <Image src={openGraphData.image} alt={openGraphData.title} fill={true}/>
-              </div>
+        <Link
+          href={`${url.trim()}?utm_medium=referral&utm_source=pagnutri&utm_campaign=nutris&utm_content=${nutriSlug.trim()}`}
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <div className={styles.bookmark_imageWrap}>
+            <div className={styles.bookmark_image}>
+              <Image src={openGraphData.image} alt={openGraphData.title} fill={true} />
             </div>
-            <div className={styles.bookmark_content}>
-              <h3>{openGraphData.title}</h3>
-            </div>
-          </Link>
-        )}
+          </div>
+          <div className={styles.bookmark_content}>
+            <h3>{openGraphData.title}</h3>
+          </div>
+        </Link>
       </div>
     </div>
   );
